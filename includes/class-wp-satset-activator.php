4f76775bb8c61534c1351cbe1a8336566f903bd8
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2023-02-09 15:56:36 +0700

    initial commit

---
+++ includes/class-wp-satset-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/agusnurwanto
+ * @since      1.0.0
+ *
+ * @package    Wp_Satset
+ * @subpackage Wp_Satset/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Satset
+ * @subpackage Wp_Satset/includes
+ * @author     Agus Nurwanto <agusnurwantomuslim@gmail.com>
+ */
+class Wp_Satset_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-satset-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/agusnurwanto
+ * @since      1.0.0
+ *
+ * @package    Wp_Satset
+ * @subpackage Wp_Satset/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Satset
+ * @subpackage Wp_Satset/includes
+ * @author     Agus Nurwanto <agusnurwantomuslim@gmail.com>
+ */
+class Wp_Satset_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
