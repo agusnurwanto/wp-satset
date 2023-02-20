d7449acb8371262bbd74d49fe9e5be913f75ba91
Andi Fajar <anfaku@yahoo.com>
2023-02-20 10:55:32 +0700

    perbaikan bug footer peta batas kecamatan

--- public/partials/wp-satset-public-peta-batas-kecamatan.php
+++ public/partials/wp-satset-public-peta-batas-kecamatan.php
@@ -5,37 +5,37 @@ $maps_all = $this->get_polygon(array(
 ));
 
 $body =  '';
-foreach($maps_all as $desa){
+foreach ($maps_all as $desa) {
     $search = $this->getSearchLocation($desa['data']);
     $body .= "
         <tr>
-            <td class='text-center'>".$desa['data']['id2012']."</td>
-            <td class='text-center'>".$desa['data']['provinsi']."</td>
-            <td class='text-center'>".$desa['data']['kabkot']."</td>
-            <td class='text-center'>".$desa['data']['kecamatan']."</td>
-            <td class='text-center'><a style='margin-bottom: 5px;' onclick='cari_alamat(\"".$search."\"); return false;' href='#' class='btn btn-danger'>Map</a></td>
+            <td class='text-center'>" . $desa['data']['id2012'] . "</td>
+            <td class='text-center'>" . $desa['data']['provinsi'] . "</td>
+            <td class='text-center'>" . $desa['data']['kabkot'] . "</td>
+            <td class='text-center'>" . $desa['data']['kecamatan'] . "</td>
+            <td class='text-center'><a style='margin-bottom: 5px;' onclick='cari_alamat(\"" . $search . "\"); return false;' href='#' class='btn btn-danger'>Map</a></td>
         </tr>
     ";
 }
 ?>
 <h1 class="text-center">Peta Batas Kecamatan<br><?php echo $this->getNamaDaerah(); ?></h1>
-<div style="width: 95%; margin: 0 auto; height: 90vh; padding-bottom: 75px;">
-    <div id="map-canvas" style="width: 100%; height: 100%;"></div>
+<div style="width: 95%; margin: 0 auto; min-height: 90vh; padding-bottom: 75px;">
+    <div id="map-canvas" style="width: 100%; height: 400px;"></div>
     <h2 class="text-center">Tabel Data</h1>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th class='text-center'>Kode Desa</th>
-                <th class='text-center'>Provinsi</th>
-                <th class='text-center'>Kabupaten/Kota</th>
-                <th class='text-center'>Kecamatan</th>
-                <th class='text-center'>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php echo $body; ?>
-        </tbody>
-    </table>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th class='text-center'>Kode Desa</th>
+                    <th class='text-center'>Provinsi</th>
+                    <th class='text-center'>Kabupaten/Kota</th>
+                    <th class='text-center'>Kecamatan</th>
+                    <th class='text-center'>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php echo $body; ?>
+            </tbody>
+        </table>
 </div>
 <script type="text/javascript">
     window.maps_all = <?php echo json_encode($maps_all); ?>;
