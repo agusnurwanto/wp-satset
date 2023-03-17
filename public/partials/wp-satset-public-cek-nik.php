f7cfae37012788634a3a7c5f323fea12c7646e08
Bagus Alvin <bagusalvin224@gmail.com>
2023-03-17 10:54:29 +0700

    Perbaikan Tabel Cek Nik

--- public/partials/wp-satset-public-cek-nik.php
+++ public/partials/wp-satset-public-cek-nik.php
@@ -45,14 +45,36 @@
                         response.data.map(function(value, index){
                             html +='<tr>';
                                 html +='<th scope="row">'+(index+1)+'</th>';
+                                html +='<td>'+value.id_p3ke+'</td>';
+                                html +='<td>'+value.kode_kemendagri+'</td>';
                                 html +='<td>'+value.nik+'</td>';
                                 html +='<td>'+value.kepala_keluarga+'</td>';
                                 html +='<td>'+value.jenis_kelamin+'</td>';
+                                html +='<td>'+value.tanggal_lahir+'</td>';
                                 html +='<td>'+value.provinsi+'</td>';
                                 html +='<td>'+value.kabkot+'</td>';
                                 html +='<td>'+value.kecamatan+'</td>';
                                 html +='<td>'+value.desa+'</td>';
                                 html +='<td>'+value.alamat+'</td>';
+                                html +='<td>'+value.Pekerjaan+'</td>';
+                                html +='<td>'+value.pendidikan+'</td>';
+                                html +='<td>'+value.rumah+'</td>';
+                                html +='<td>'+value.punya_tabungan+'</td>';
+                                html +='<td>'+value.jenis_desil+'</td>';
+                                html +='<td>'+value.jenis_atap+'</td>';
+                                html +='<td>'+value.jenis_dinding+'</td>';
+                                html +='<td>'+value.jenis_lantai+'</td>';
+                                html +='<td>'+value.sumber_penerangan+'</td>';   
+                                html +='<td>'+value.bahan_bakar_memasak+'</td>';   
+                                html +='<td>'+value.sumber_air_minum+'</td>';
+                                html +='<td>'+value.fasilitas_bab+'</td>';
+                                html +='<td>'+value.penerima_bpnt+'</td>';   
+                                html +='<td>'+value.penerima_bpum+'</td>';   
+                                html +='<td>'+value.penerima_bst+'</td>';
+                                html +='<td>'+value.penerima_pkh+'</td>';
+                                html +='<td>'+value.penerima_sembako+'</td>';
+                                html +='<td>'+value.resiko_stunting+'</td>';
+                                // html +='<td>'+value.padan_dukcapil+'</td>';
                             html +='</tr>';
                         })
                         var pesan = ''
@@ -60,14 +82,36 @@
                                 +'<thead>'
                                     +'<tr>'
                                         +'<th class="text-center" style="width: 20px;">No</th>'
+                                        +'<th class="text-center">Id P3KE</th>'
+                                        +'<th class="text-center">Kode Kemendagri</th>'
                                         +'<th class="text-center">NIK</th>'
                                         +'<th class="text-center">Kepala Keluarga</th>'
                                         +'<th class="text-center">Jenis Kelamin</th>'
+                                        +'<th class="text-center">Tanggal Lahir</th>'
                                         +'<th class="text-center">Provinsi</th>'
                                         +'<th class="text-center">Kabupaten / Kota</th>'
                                         +'<th class="text-center">Kecamatan</th>'
                                         +'<th class="text-center">Desa</th>'
                                         +'<th class="text-center">Alamat</th>'
+                                        +'<th class="text-center">Pekerjaan</th>'
+                                        +'<th class="text-center">Pendidikan</th>'
+                                        +'<th class="text-center">Rumah</th>'
+                                        +'<th class="text-center">Punya Tabungan</th>'
+                                        +'<th class="text-center">Jenis Desil</th>'
+                                        +'<th class="text-center">Jenis Atap</th>'
+                                        +'<th class="text-center">Jenis Dinding</th>'
+                                        +'<th class="text-center">Jenis Lantai</th>'
+                                        +'<th class="text-center">Sumber Penerangan</th>'   
+                                        +'<th class="text-center">Bahan Bakar Memasak</th>'   
+                                        +'<th class="text-center">Sumber Air Minum</th>'
+                                        +'<th class="text-center">Fasilitas BAB</th>'
+                                        +'<th class="text-center">Penerima Bpnt</th>'   
+                                        +'<th class="text-center">Penerima Bpum</th>'   
+                                        +'<th class="text-center">Penerima Bst</th>'
+                                        +'<th class="text-center">Penerima Pkh</th>'
+                                        +'<th class="text-center">Penerima Sembako</th>'
+                                        +'<th class="text-center">Resiko Stunting</th>'
+                                        // +'<th class="text-center">Padan Dukcapil</th>'
                                     +'</tr>'
                                 +'</thead>'
                                 +'<tbody>'
